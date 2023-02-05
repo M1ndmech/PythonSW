a94c5d220aa619f6aff1c58bf11fbd239ced315c
Mindmech <alex8008@bk.ru>
2023-02-05 10:52:41 +0300

    s4 in progress

---
+++ s4/main.py
@@ -0,0 +1,50 @@
+'''
+1. Напишите программу, которая принимает на вход строку, и отслеживает, сколько раз каждый символ уже встречался. 
+Количество повторов добавляется к символам с помощью постфикса формата _n. 
+Input: a a a b c a a d c d d 
+Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+'''
+
+def input_list (length):
+    list = []
+    for i in range (length):
+        input_symbol = input('Введите символ: ')
+        list.append(input_symbol)
+    return list
+
+length_input = int(input("Введите длину списка: "))
+list1 = input_list(length_input)
+set1 = set(list1)
+
+for item in set1:
+    print(f'{item}: {list1.count(item)}')
+
+
+
+'''
+2. Пользователь вводит текст(строка). 
+Словом считается последовательность непробельных символов идущих подряд, слова разделены одним или большим числом пробелов. 
+Определите, сколько различных слов содержится в этом тексте. 
+
+Input: She sells sea shells on the sea shore 
+The shells that she sells are sea shells I'm sure.
+So if she sells sea shells on the sea shore 
+I'm sure that the shells are sea shore shells 
+Output: 13'''
+
+str_test = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells" 
+str_test2 = str_test.lower()
+set2 = set(str_test2.split(" "))
+print(set2)
+print(len(set2))
+
+'''
+3. Ваня и Петя поспорили, кто быстрее решит следующую задачу: 
+“Задана последовательность неотрицательных целых чисел. 
+Требуется определить значение наибольшего элемента последовательности, которая завершается 
+первым встретившимся нулем (число 0 не входит в последовательность)”. 
+Однако 2  друга оказались не такими смышлеными. 
+Никто из ребят не смог до конца сделать это задание. 
+Они решили так: у кого будет меньше ошибок в коде, тот и выиграл спор. 
+За помощью товарищи обратились к Вам, студентам.
+'''

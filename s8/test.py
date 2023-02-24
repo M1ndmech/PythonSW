cd5a7852fda900b8179d8b5ac778f35d79c29ad8
Mindmech <alex8008@bk.ru>
2023-02-25 00:30:58 +0300

    + editing

--- s8/test.py
+++ s8/test.py
@@ -11,6 +11,12 @@ def read_file(file) -> dict:                        #считывает файл
             contacts_dict.update(dict_line)         #дополнение преобразованной строкой (парой элементов) словаря
     return contacts_dict
 
+def write_dict(nested_dict, file):                        #считывает файл и записывает содержимое в переменную
+    with open(file, 'w', encoding="utf-8") as f:
+        for k, v in nested_dict.items():
+            f.writelines(f'\n{{"{k}":{v}}}')
+    print('Файл перезаписан')
+
 def print_nested_dict(nested_dict, keylist):        #навести красоту по табуляции, одного \t не хватает
     for key, value in nested_dict.items():
         for arg in value.keys():
@@ -38,17 +44,29 @@ def lookup_contact (nested_dict):
             input1 = input("Введите имя:")
             for key, value in nested_dict.items():
                 if nested_dict[key]['name'] == input1:
-                   print(*nested_dict[key].values())
+                   print(f'Найдена запись: {" ".join(str(x) for x in nested_dict[key].values())}')
             return
         elif input_choice == 2:
             input2 = input("Введите фамилию:")
             for key, value in nested_dict.items():
                 if nested_dict[key]['surname'] == input2:
-                    print(*nested_dict[key].values())
+                    print(f'Найдена запись: {" ".join(str(x) for x in nested_dict[key].values())}')
             return
         else:
             return
 
+def change_contact (nested_dict, keylist, file):    
+    # Условно работает - изменяет, но добавляет строку в начало файла.
+    # И надо нажимать 2 несколько раз если есть повторы имен/фамилий
+    input1 = input("Поиск изменяемого контакта. Введите фамилию или имя:")
+    for key, value in nested_dict.items():
+        if nested_dict[key]['name'] == input1 or nested_dict[key]['surname'] == input1:
+            print(f'Найдена запись: {" ".join(str(x) for x in nested_dict[key].values())}') 
+            while(input_choice_2:= int(input('Выбран правильный контакт? (1 - Да, 2 - Нет):'))) != 2:
+                if input_choice_2 == 1:
+                    for k, v in keylist.items():
+                        nested_dict[key][k] = (input_2 := input(f'Введите новое значение {v} или нажмите Enter:'))
+    write_dict(nested_dict, file)
 
 # print(contacts_dict['1']['name'])
 
@@ -60,7 +78,7 @@ def user_action():
     '1) Вывести весь справочник\n'
     '2) Добавить контакт \n'
     '3) Найти контакт\n'
-    # '4) Найти и удалить контакт\n'
+    '4) Изменить контакт\n'
     )
     while (input1:= int(input('Выберите действие со справочником (0 = выход):'))) != 0:
         if input1 == 1:
@@ -69,8 +87,8 @@ def user_action():
             append_contact(contacts_file, cont_arg_list, cont_num_list)
         elif input1 == 3:
             lookup_contact(contacts_dict)
-        # elif input1 == 4:
-        #     remove_contact(contacts_file)
+        elif input1 == 4:
+            change_contact(contacts_dict, cont_arg_list, contacts_file)
         else:
             print("Некорректный ввод")
 

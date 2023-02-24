35af55e85fed8b3144453c14a6bf0f34a6c743a9
Mindmech <alex8008@bk.ru>
2023-02-24 22:25:11 +0300

    search done

--- s8/test.py
+++ s8/test.py
@@ -28,6 +28,28 @@ def append_contact(file, keylist, cont_numbers):    #добавление стр
         f.write(f'\n{save_line}')
     print ('Контакт добавлен')
 
+def lookup_contact (nested_dict):
+    print ('Поиск по:\n'
+    '1) Имени\n'
+    '2) Фамилии'
+    )
+    while (input_choice:= int(input('Выберите действие (0 = выход):'))) != 0:
+        if input_choice == 1:
+            input1 = input("Введите имя:")
+            for key, value in nested_dict.items():
+                if nested_dict[key]['name'] == input1:
+                   print(*nested_dict[key].values())
+            return
+        elif input_choice == 2:
+            input2 = input("Введите фамилию:")
+            for key, value in nested_dict.items():
+                if nested_dict[key]['surname'] == input2:
+                    print(*nested_dict[key].values())
+            return
+        else:
+            return
+
+
 # print(contacts_dict['1']['name'])
 
 def user_action():
@@ -37,7 +59,7 @@ def user_action():
     print ('Добро пожаловать! \n'
     '1) Вывести весь справочник\n'
     '2) Добавить контакт \n'
-    # '3) Найти контакт\n'
+    '3) Найти контакт\n'
     # '4) Найти и удалить контакт\n'
     )
     while (input1:= int(input('Выберите действие со справочником (0 = выход):'))) != 0:
@@ -45,8 +67,8 @@ def user_action():
             print_nested_dict(contacts_dict, cont_arg_list)
         elif input1 == 2:
             append_contact(contacts_file, cont_arg_list, cont_num_list)
-        # elif input1 == 3:
-        #     print(search_contact(contacts_file))
+        elif input1 == 3:
+            lookup_contact(contacts_dict)
         # elif input1 == 4:
         #     remove_contact(contacts_file)
         else:
